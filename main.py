86a59dabbeb4cf026736cadfd58f6e425f8418ad
nmeier00 <nicholasmeier@unomaha.edu>
2025-04-18 03:34:11 +0000

    Done with lab

--- main.py
+++ main.py
@@ -1,4 +1,48 @@
 #MapPlot.py
-#Name:
-#Date:
-#Assignment:
+#Name: nick meier
+#Date: april 17, 2025
+#Assignment: lab 10
+
+import aids
+import pandas
+import matplotlib.pyplot as plt
+report = aids.get_report()
+
+yearsAngola = []
+prevalencesAngola = []
+prevalencesZimbabwe =[]
+prevalencesBotswana = []
+
+for r in report:
+    country = r["Country"]
+    year = r["Year"]
+    prevalence = r["Data"]["HIV Prevalence"]["Adults"]
+    if country == 'Angola':
+        yearsAngola.append(year)
+        prevalencesAngola.append(prevalence)
+    #print(years, country, prevalence)
+
+
+for r in report:
+    country = r["Country"]
+    year = r["Year"]
+    prevalence = r["Data"]["HIV Prevalence"]["Adults"]
+    if country == 'Zimbabwe':
+        prevalencesZimbabwe.append(prevalence)
+
+for r in report:
+    country = r["Country"]
+    year = r["Year"]
+    prevalence = r["Data"]["HIV Prevalence"]["Adults"]
+    if country == 'Botswana':
+        prevalencesBotswana.append(prevalence)
+    
+df = pandas.DataFrame({"Year Angola": yearsAngola, "Adult HIV Prevalence Angola": prevalencesAngola,
+                        "Adult HIV Prevalence Zimbabwe": prevalencesZimbabwe,
+                        "Adult HIV Prevalence Botswana": prevalencesBotswana})
+
+
+df.plot(kind = 'line', x = 'Year Angola', y = ['Adult HIV Prevalence Angola', 'Adult HIV Prevalence Zimbabwe', 'Adult HIV Prevalence Botswana'])
+
+plt.savefig("output")
+
